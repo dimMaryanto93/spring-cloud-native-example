35623e22f57a23e4ba51042da3ffc9973384808e
dimasm93 <software.dimas_m@icloud.com>
2022-08-02 15:02:34 +0700

    init project

---
+++ src/test/java/com/maryanto/dimas/example/SpringCloudNativeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNativeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
